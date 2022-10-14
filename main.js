56530b859a5f5c2276fa466c92842e0c11923d01
JeffreyWithSAM <107282450+JeffreyWithSAM@users.noreply.github.com>
2022-10-13 19:00:01 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+//  | This is the main javascript file for use with the Insurabook CRM
+//  | 
+//  | 
+//  | Date: 10/13/2022
+//  | Author(s): Jeffrey Robinson II
+
+//  | This if statement can check for text within the current url
+//  | The '-1' means 'not found'
+
+if (document.location.href.indexOf('Mode=edit') > -1){ 
+    //Your code goes here
+}
