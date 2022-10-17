d8d42d3e78af7dab6df804f0da701abb6ffbc5db
JeffreyWithSAM <107282450+JeffreyWithSAM@users.noreply.github.com>
2022-10-17 15:31:58 -0500

    Update champ.js

--- champ.js
+++ champ.js
@@ -1,8 +1,10 @@
-//  | This is the main javascript file for use with the Insurabook CRM
-//  | 
-//  | 
-//  | Date: 10/13/2022
-//  | Author(s): Jeffrey Robinson II
+//   ___________________________________________________________________
+//  | This is the main javascript file for use with the CHAMP CRM       |
+//  |                                                                   |
+//  |                                                                   |
+//  | Date: 10/13/2022                                                  |
+//  | Author(s): Jeffrey Robinson II                                    |
+//  |___________________________________________________________________|
 
 //  | This if statement can check for text within the current url
 //  | The '-1' means 'not found'
@@ -14,9 +16,22 @@ function urlContains() {
 }
 
 function main() {
+    // | Find and replace the text for the sidebar shortcut for 'Opportunities' to 'Dashboards'
     let intervalID  = setInterval((event) => {
         let o = document.getElementById('sb_opportunities').children[1];
         o.innerHTML = 'Dashboards';
+    }, 10);
+    setInterval((event) => {
+        if (!!document.getElementById('sb_opportunities')) {
+            finished = true;
+            clearInterval(intervalID);
+        }
+    }, 10);
+    
+    // | Find and replace the text for 'Opportunities' to 'Dashboards'
+    let intervalID  = setInterval((event) => {
+        let p = document.getElementById('').children[1];
+        p.innerHTML = 'Dashboards';
     }, 10);
     setInterval((event) => {
         if (!!document.getElementById('sb_opportunities')) {
