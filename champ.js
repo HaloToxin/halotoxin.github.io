c3a014457c6cf7b703dc4db3fa0f27f2f2bc2239
JeffreyWithSAM <107282450+JeffreyWithSAM@users.noreply.github.com>
2022-10-17 12:26:08 -0500

    Update champ.js

--- champ.js
+++ champ.js
@@ -1,1 +1,23 @@
+//  | This is the main javascript file for use with the Insurabook CRM
+//  | 
+//  | 
+//  | Date: 10/13/2022
+//  | Author(s): Jeffrey Robinson II
 
+//  | This if statement can check for text within the current url
+//  | The '-1' means 'not found'
+
+function urlContains() {
+    if (document.location.href.indexOf('Target') > -1){ 
+    //Your code goes here
+    }
+}
+
+function main() {
+    setInterval((event) => {
+        let o = document.getElementById('sb_opportunities').children[1];
+        o.innerHTML = 'Dashboards';
+    }, 1000)
+}
+
+main();
