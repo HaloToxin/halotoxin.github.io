a9aefcdea298a2badc3e4ea6d5ffeb8404bc2583
JeffreyWithSAM <107282450+JeffreyWithSAM@users.noreply.github.com>
2022-11-10 10:26:16 -0600

    Update insurabook.js

--- insurabook.js
+++ insurabook.js
@@ -13,7 +13,18 @@ function urlContains() {
     }
 }
 
+function favicon() {
+    var link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.getElementsByTagName('head')[0].appendChild(link);
+    }
+    link.href = 'https://jeffreywithsam.github.io/ibfavicon.ico';
+}
+
 function main() {
+    favicon();
 }
 
 main();
