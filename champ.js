7ab2f14fc14d6c7a3ba20614eaba4924dbc73744
HaloToxin <49005565+HaloToxin@users.noreply.github.com>
2022-11-28 13:46:00 -0600

    Update champ.js

--- champ.js
+++ champ.js
@@ -24,7 +24,7 @@ function main() {
         document.getElementById('sb_opportunities').children[1].innerHTML = 'Dashboards';
         
         // | Find and replace the text for the navbar header from 'Opportunities' to 'Dashboards'
-        let o = document.querySelector("div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > div:nth-child(2) > header > div.flex.flex-row.justify-start.items-center.topmenu-nav > div");
+        let o = document.querySelector("div.topmenu-navtitle");
         if (o.innerHTML.indexOf("Opportunities") !== -1){
             o.innerHTML = "Dashboard"
         }
