ddea90f83ec71dc1a42f7b1d76dfa13a046028e3
JeffreyWithSAM <107282450+JeffreyWithSAM@users.noreply.github.com>
2022-11-28 11:58:37 -0600

    Update champ.js

--- champ.js
+++ champ.js
@@ -16,10 +16,18 @@ function urlContains(String target) {
 }
 
 function main() {
-    // | Find and replace the text for the sidebar shortcut for 'Opportunities' to 'Dashboards'
+    // | This interval is used to change details about the CRM as they load
     let intervalID  = setInterval((event) => {
-        let o = document.getElementById('sb_opportunities').children[1];
-        o.innerHTML = 'Dashboards';
+        
+        // | Find and replace the text for the sidebar shortcut for 'Opportunities' to 'Dashboards'
+        document.getElementById('sb_opportunities').children[1].innerHTML = 'Dashboards';
+        
+        // | Find and replace the text for the navbar header from 'Opportunities' to 'Dashboards'
+        let o = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > div:nth-child(2) > header > div.flex.flex-row.justify-start.items-center.topmenu-nav > div");
+        if (o.innerHTML = " Opportunities "){
+            o.innerHTML = " Dashboard "
+        }
+        
     }, 10);    
 }
 
