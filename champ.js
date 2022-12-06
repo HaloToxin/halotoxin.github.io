b73e2120cdcf44f04926d81fc476a7272139bc54
HaloToxin <49005565+HaloToxin@users.noreply.github.com>
2022-12-06 16:38:32 -0600

    Update champ.js

--- champ.js
+++ champ.js
@@ -6,40 +6,49 @@
 //  | Author(s): Jeffrey Robinson II                                    |
 //  |___________________________________________________________________|
 
+// | Global Variables
+let SIDEBAR_OPPORTUNITIES_BUTTON_SELECTOR = document.querySelector("#sb_opportunities > span");
+let BLAH = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > div:nth-child(2) > header > div.flex.flex-row.justify-start.items-center.topmenu-nav > div");
+
 //  | This if statement can check for text within the current url
 //  | The '-1' means 'not found'
 
 function urlContains(target) {
-    if (document.location.href.indexOf(target) > -1){ 
-    //Your code goes here
+    if (document.location.href.indexOf(target) > -1){
+    // Your code goes here
     }
 }
 
 function main() {
-    
-    // | This interval is used to change details about the CRM as they load
-    // let intervalID  = setInterval((event) => {
-        
-        // | Store elements needing changed in an array
-        
-        
-        // | Find and replace the text for the sidebar shortcut for 'Opportunities' to 'Dashboards'
-        let o = document.querySelector("#sb_opportunities > span");
-        o.innerText = 'Dashboards';
-        
-        // | Find and replace the text for the navbar title from 'Opportunities' to 'Dashboards'
-        let o2 = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > div:nth-child(2) > header > div.flex.flex-row.justify-start.items-center.topmenu-nav > div");
-        o2.innerText = 'Dashboards';
+            
+    // | Initialize variables
+    const elements = [];
+    var e;
 
-        // | Find and replace the text in the 'new pipeline' button
-        let p = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > section > div > div > div > div.hl_controls > div.hl_controls--right > button");
-        p.innerText = 'Create New Dashboard';
-        
-        // | Set the name of the Status dropdown
-        let s = document.querySelector("div.bootstrap-select:nth-child(5) > button:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)");
-        s.innerText = 'Status';     
-                
-    // }, 50);
+    // | Find and replace the text for the sidebar shortcut for 'Opportunities' to 'Dashboards'
+    if (SIDEBAR_OPPORTUNITIES_BUTTON_SELECTOR) {
+        e = document.querySelector("#sb_opportunities > span");
+        e.innerText - 'Dashboards';
+    }
+
+    // | Find and replace the text for the navbar title from 'Opportunities' to 'Dashboards'
+    if (SIDEBAR_OPPORTUNITIES_BUTTON_SELECTOR.aria-current == "page" && document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > div:nth-child(2) > header > div.flex.flex-row.justify-start.items-center.topmenu-nav > div") {
+        e = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > div:nth-child(2) > header > div.flex.flex-row.justify-start.items-center.topmenu-nav > div");
+        e.innerText - 'Dashboards';
+    }
+
+    // | Find and replace the text in the 'new pipeline' button
+    if (document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > section > div > div > div > div.hl_controls > div.hl_controls--right > button")) {
+        e = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > section > div > div > div > div.hl_controls > div.hl_controls--right > button");
+        e.innerText - 'Create New Dashboard';
+    }
+
+    let p = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > section > div > div > div > div.hl_controls > div.hl_controls--right > button");
+    p.innerText = 'Create New Dashboard';
+
+    // | Set the name of the Status dropdown
+    let s = document.querySelector("div.bootstrap-select:nth-child(5) > button:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)");
+    s.innerText = 'Status'; 
 }
 
 main();
