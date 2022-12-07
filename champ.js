87cfbd2dfcd02a2576a908df23e764dc694599c5
HaloToxin <49005565+HaloToxin@users.noreply.github.com>
2022-12-07 10:16:11 -0600

    added replacements for the top navbar tabs

--- champ.js
+++ champ.js
@@ -10,7 +10,7 @@
 //  | The '-1' means 'not found'
 
 function urlContains(target) {
-    if (document.location.href.indexOf(target) > -1){ 
+    if (document.location.href.indexOf(target) > -1){
     //Your code goes here
     }
 }
@@ -24,20 +24,23 @@ function main() {
         
         
         // | Find and replace the text for the sidebar shortcut for 'Opportunities' to 'Dashboards'
-        let o = document.querySelector("#sb_opportunities > span");
-        o.innerText = 'Dashboards';
+        let e = document.querySelector("#sb_opportunities > span");
+        e.innerText = 'Dashboards';
         
         // | Find and replace the text for the navbar title from 'Opportunities' to 'Dashboards'
-        let o2 = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > div:nth-child(2) > header > div.flex.flex-row.justify-start.items-center.topmenu-nav > div");
-        o2.innerText = 'Dashboards';
+        let e = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > div:nth-child(2) > header > div.flex.flex-row.justify-start.items-center.topmenu-nav > div");
+        e.innerText = 'Dashboards';
 
         // | Find and replace the text in the 'new pipeline' button
-        let p = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > section > div > div > div > div.hl_controls > div.hl_controls--right > button");
-        p.innerText = 'Create New Dashboard';
-        
-        // | Set the name of the Status dropdown
-        let s = document.querySelector("div.bootstrap-select:nth-child(5) > button:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)");
-        s.innerText = 'Status';     
+        let e = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > section > div > div > div > div.hl_controls > div.hl_controls--right > button");
+        e.innerText = 'Create New Dashboard';
+
+        // | Find and replace the text in the 'Opportunities' header
+        let e = document.querySelector("#tb_opportunities-tab > span:nth-child(1)");
+        e.innerText = 'View';
+
+        let e = document.querySelector("#tb_pipeline > span:nth-child(1)");
+        e.innerText = 'Edit';
                 
     }, 50);
 }
