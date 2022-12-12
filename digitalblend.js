b6b79e9a243feff973be5b53ca2e7757136fb12e
HaloToxin <49005565+HaloToxin@users.noreply.github.com>
2022-12-12 16:58:35 -0600

    Update digitalblend.js

--- digitalblend.js
+++ digitalblend.js
@@ -1,1 +1,23 @@
+//   _____________________________________________________________________________________________________________
+//  | This is the main javascript file for use with the Digital Blend Video Leads HighLevel Delivery system       |
+//  |                                                                                                             |
+//  |                                                                                                             |
+//  | Date: 12/12/2022                                                                                            |
+//  | Author(s): Jeffrey Robinson II                                                                              |
+//  |_____________________________________________________________________________________________________________|
+//
 
+//  | This if statement checks for text within the current url
+//  | The '-1' means 'not found'
+
+function urlContains(target) {
+    if (document.location.href.indexOf(target) > -1){
+    //Your code goes here
+    }
+}
+
+function main() {
+  
+}
+
+main();
