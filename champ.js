b5e588529f6fe0e310fa3d9d5d9543c2c0d29bf4
HaloToxin <49005565+HaloToxin@users.noreply.github.com>
2022-12-14 16:37:16 -0600

    declare e outside of interval

--- champ.js
+++ champ.js
@@ -15,10 +15,12 @@ function urlContains(target) {
     }
 }
 // | This interval is used to change details about the CRM as they load
+let e;
+
 let intervalID  = setInterval((event) => {
 
     // | Find and replace the text for the sidebar shortcut for 'Opportunities' to 'Dashboards'
-    let e = document.querySelector("#sb_opportunities > span:nth-child(2)");
+    e = document.querySelector("#sb_opportunities > span:nth-child(2)");
     e.innerText = 'Dashboards';
 
     // | Find and replace the text for the navbar title from 'Opportunities' to 'Dashboards'
