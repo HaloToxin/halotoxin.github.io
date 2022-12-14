e267a7a74f81c6ff6d0a574b11da797926577877
HaloToxin <49005565+HaloToxin@users.noreply.github.com>
2022-12-14 11:34:04 -0600

    Update champ.js

--- champ.js
+++ champ.js
@@ -14,36 +14,34 @@ function urlContains(target) {
     //Your code goes here
     }
 }
+// | This interval is used to change details about the CRM as they load
+let intervalID  = setInterval((event) => {
 
-function main() {
+    // | Find and replace the text for the sidebar shortcut for 'Opportunities' to 'Dashboards'
+    let e = document.querySelector("#sb_opportunities > span");
+    e.innerText = 'Dashboards';
+
+    // | Find and replace the text for the navbar title from 'Opportunities' to 'Dashboards'
+    //let e = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > div:nth-child(2) > header > div.flex.flex-row.justify-start.items-center.topmenu-nav > div");
+    //e.innerText = 'Dashboards';
+
+    // | Find and replace the text in the 'new pipeline' button
+    //let e = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > section > div > div > div > div.hl_controls > div.hl_controls--right > button");
+    //e.innerText = 'Create New Dashboard';
+
+    // | Find and replace the text in the 'Opportunities' navbar header
+    //let e = document.querySelector("#tb_opportunities-tab > span:nth-child(1)");
+    //e.innerText = 'View';
+
+    // | Find and replace the text in the 'Pipelines' navbar header
+    //let e = document.querySelector("#tb_pipeline > span:nth-child(1)");
+    //e.innerText = 'Edit';
+
+}, 50);
+
+// | Kill the interval that is making the changes, after a reasonable amount of time has passed for the changes to be made: ***[10 SECONDS]***
+setTimeout(() => {
     
-    // | This interval is used to change details about the CRM as they load
-    let intervalID  = setInterval((event) => {
-        
-        // | Store elements needing changed in an array
-        
-        
-        // | Find and replace the text for the sidebar shortcut for 'Opportunities' to 'Dashboards'
-        let e = document.querySelector("#sb_opportunities > span");
-        e.innerText = 'Dashboards';
-        
-        // | Find and replace the text for the navbar title from 'Opportunities' to 'Dashboards'
-        let e = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > div:nth-child(2) > header > div.flex.flex-row.justify-start.items-center.topmenu-nav > div");
-        e.innerText = 'Dashboards';
-
-        // | Find and replace the text in the 'new pipeline' button
-        let e = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > section > div > div > div > div.hl_controls > div.hl_controls--right > button");
-        e.innerText = 'Create New Dashboard';
-
-        // | Find and replace the text in the 'Opportunities' navbar header
-        let e = document.querySelector("#tb_opportunities-tab > span:nth-child(1)");
-        e.innerText = 'View';
-
-        // | Find and replace the text in the 'Pipelines' navbar header
-        let e = document.querySelector("#tb_pipeline > span:nth-child(1)");
-        e.innerText = 'Edit';
-                
-    }, 50);
-}
-
-main();
+    clearInterval(intervalID);
+    
+}, 10000);
