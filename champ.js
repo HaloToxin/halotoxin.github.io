ead9e655c27f3e9a69b5ab1fedd1ef960907bad0
HaloToxin <49005565+HaloToxin@users.noreply.github.com>
2022-12-14 16:04:06 -0600

    uncommented second change in main interval and changed e declaration to reassignment

--- champ.js
+++ champ.js
@@ -22,8 +22,8 @@ let intervalID  = setInterval((event) => {
     e.innerText = 'Dashboards';
 
     // | Find and replace the text for the navbar title from 'Opportunities' to 'Dashboards'
-    //let e = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > div:nth-child(2) > header > div.flex.flex-row.justify-start.items-center.topmenu-nav > div");
-    //e.innerText = 'Dashboards';
+    e = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > div:nth-child(2) > header > div.flex.flex-row.justify-start.items-center.topmenu-nav > div");
+    e.innerText = 'Dashboards';
     
     // | Find and replace the text in the 'new pipeline' button
     //let e = document.querySelector("#app > div.flex.v2-open.sidebar-v2-location.iy3DiTzRf9NBlWxVqWyi.flex.v2-open.sidebar-v2-location > section > div > div > div > div.hl_controls > div.hl_controls--right > button");
