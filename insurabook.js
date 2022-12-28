6c6690a74f2fc3a17f334e98a92ca22e603cd743
HaloToxin <49005565+HaloToxin@users.noreply.github.com>
2022-12-28 14:56:35 -0600

    uncommented className override

--- insurabook.js
+++ insurabook.js
@@ -40,7 +40,7 @@ function main() {
     // Change the logo for the Digital Blend Leads subaccount
     if (urlContains("TR2M2XqUylHeOoLksd0z")) {
         var logo = document.querySelector("img.object-contain.agency-logo");
-        // logo.className = "custom-logo-element";
+        logo.className = "custom-logo-element";
     }
 }
 
