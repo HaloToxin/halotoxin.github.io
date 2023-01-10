708146f8aa2055d89ef51fc81cff5366d4706e30
HaloToxin <49005565+HaloToxin@users.noreply.github.com>
2023-01-10 11:46:43 -0600

    comments

--- champ.js
+++ champ.js
@@ -29,6 +29,7 @@ let interval1  = setInterval((event) => {
         e.innerText = 'Dashboards';
     }
 
+    // Replace stuff when looking at a card in a dashboard
     try {
         e = document.querySelector("div.modal:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > h2:nth-child(1)");
         let f = document.querySelector("div.col-sm-6:nth-child(6) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > input:nth-child(3)");
