d3d3ddd088212b11e035c6a21c71e57f66bb1934
HaloToxin <49005565+HaloToxin@users.noreply.github.com>
2023-01-11 16:48:28 -0600

    testing

--- champ.js
+++ champ.js
@@ -42,11 +42,12 @@ let interval1  = setInterval((event) => {
     try {
         if (!document.getElementById("FC_Icon_Link")) {
             e = document.querySelector("div.py-3:nth-child(6) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > input:nth-child(3)");
-            let f = e.parentNode;
+            console.log(e.value);
+            //let f = e.parentNode;
             let a = document.createElement('a');
             a.href = e.value;
             a.id = "FC_Icon_Link";
-            f.appendChild(a);
+            //f.appendChild(a);
         }
     } catch (error) {
         console.error(error);
