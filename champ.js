08fbe43e1a5cae512d2f05a121f5bb85803e91c6
HaloToxin <49005565+HaloToxin@users.noreply.github.com>
2023-01-16 16:20:19 -0600

    FC LINK onClick arrow function

--- champ.js
+++ champ.js
@@ -41,9 +41,10 @@ let interval1  = setInterval((event) => {
     // OnClick function for the FC Link
     try {
         e = document.querySelector("div.py-3:nth-child(6) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > input:nth-child(3)");
-        e.onclick => () {
+        e.onClick = () => {
             window.open(e.value, '_blank');
-        };
+        }
     } catch (error) {
         console.error("FC Link OnClick Error");
     }
+    
