ee416e0368e2f0b86bf97082ffdaf5ed8a116182
HaloToxin <49005565+HaloToxin@users.noreply.github.com>
2023-02-09 10:43:01 -0600

    undo all my work to keep insurabook branding

--- insurabook.js
+++ insurabook.js
@@ -34,43 +34,6 @@ function favicon(url) {
 
 function main() {
     favicon('https://halotoxin.github.io/IB-Favicon.png');
-
-    // Change the logo for the Digital Blend Leads subaccount @JakeWithSAM
-    const targets = [
-        'TR2M2XqUylHeOoLksd0z', // Video Blend Leads - VBL Snapshot Source
-        'E8fz7WJmRxiBGMZAqVkU', // Evelynn Francisco
-        's4RXQeAgtig0RLhoD9SB', // Donovan March - Synchronized Agency
-        'YWifbVzd6shHgcL7mZeB', // Keri McCallister - Horse Creek Insurance
-        'wTS2r9AFomCOfr863Rve', // Joshua Rockwood - Rocket Growth Strategies
-        '9sxFwZ4WD4aqNXkejNZw', // Alex Jackson - The Jackson Agency
-        'B8eUe9X61z05pHPFKJxf', // VBL Test Account
-    ];
-
-    const logoChanger = setInterval((event) => {
-        try {
-            var logo = document.querySelector("img.object-contain.agency-logo");
-            if (urlContains(targets)) {
-                logo.src = "https://halotoxin.github.io/DBL-Blender.png";
-                logo.id="dbl-logo";
-            }
-        } catch (error) {
-            console.log("Searching for Logo...");
-        }
-        
-    }, 50);
-
-    const titleChanger = setInterval((event) => {
-        try {
-            var title = document.getElementsByTagName('title')[0];
-            console.log(title);
-            if (urlContains(targets)) {
-                title.innerText = "Video Blended Leads";
-                clearInterval(titleChanger);
-            }
-        } catch (error) {
-            console.log("Searching for Title...");
-        }
-    }, 50);
 }
 
 main();
